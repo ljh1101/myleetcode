1369949a05cf2ab8e701493fe5c6ed28712236b6
Liu jiahui <liujiahui19@huawei.com>
2023-03-09 23:40:25 +0800

    2023.3.9

--- tree/106_buildTree.cpp
+++ tree/106_buildTree.cpp
@@ -15,10 +15,29 @@ struct TreeNode
     }
 };
 
-
 class Solution {
-public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+  public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
+    {
+        if (postorder.size() == 0) {
+            return nullptr;
+        }
+        int rootVal = postorder[postorder.size() - 1];
+        TreeNode* root = new TreeNode(rootVal);
 
+        int index = 0;
+        for (; index < inorder.size(); index++)
+            if (inorder[index] == rootVal)
+                break;
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder(inorder.begin() + index + 1, inorder.end());
+
+        postorder.resize(postorder.size() - 1);
+        vector<int> leftPostorder(postorder.begin(), postorder.begin() + index);
+        vector<int> rightPostorder(postorder.begin() + index, postorder.end());
+        root->left = buildTree(leftInorder, leftPostorder);
+        root->right = buildTree(rightInorder, rightPostorder);
+
+        return root;
     }
 };
